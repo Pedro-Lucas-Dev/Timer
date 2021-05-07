fb42bd080bc7440ce2c8a48a00f0293846652027
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-05-06 21:44:30 -0300

    feat: create array and randomize array in screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [time, setTime] = useState(null);
   const date = new Date();
+  const [timeBounded, setTimebounded] = useState(300);
+  const [running, setRunning] = useState(false);
+
+  const words = [
+    "True ",
+    "False ",
+    "Magic ",
+    "No ",
+    "Yes ",
+    "Game",
+    "AAA",
+    "BBB",
+    "A",
+  ];
+  const randomWords = words[Math.floor(Math.random() * words.length)];
+
   const mouths = [
     "Janeiro",
     "Fevereiro",
@@ -19,22 +34,31 @@ function App() {
     "Novembro",
     "Dezembro",
   ];
-  useEffect(() => {
-    setInterval(() => {
-      const refreshDate = new Date();
-      setTime(refreshDate.getSeconds());
-    }, 1000);
-  }, []);
+
+  const startTimeOut = running
+    ? setTimeout(() => {
+        updateTime();
+      }, 1000)
+    : null;
+
+  const updateTime = () => {
+    let decrement = timeBounded - 1;
+    setTimebounded(decrement);
+  };
 
   return (
     <div className="App">
+      <h1>{randomWords}</h1>
+      <h1>
+        {Math.floor(timeBounded / 60)}:{timeBounded % 60}{" "}
+      </h1>
+      <button onClick={() => setRunning(true)}>Start</button>
+      <button onClick={() => setRunning(false)}>Stop</button>
       <h2>Hoje é dia {date.getDate()}</h2>
       <h2>Mês {mouths[date.getMonth()]}</h2>
       <h2>Do ano {date.getFullYear()}</h2>
       <h2>hora: {date.getHours()}</h2>
       <h2>minuto: {date.getMinutes()}</h2>
-      <h2>Segundos: {time}</h2>
-
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
